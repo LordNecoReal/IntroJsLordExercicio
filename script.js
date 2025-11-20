ed73912ffbe1249c29482be14926009f97ef3c41
LordNecoReal <lordconhecimento@gmail.com>
2025-11-20 15:52:15 -0300

    first commit

---
+++ script.js
@@ -0,0 +1,44 @@
+//Variaveis Let: 
+
+let Nome = "Lord";
+
+let Sobrenome = "Neco Real";
+
+let Idade = 32
+
+let Email = "lordforte@email.com";
+
+let NúmerodeCelular = (21999997621);
+
+let Endereço = "Rua encantada, 28";
+
+let BebidaFavorita = "Suco de morango";
+
+//Console.log:
+
+console.log(Nome);
+console.log(Sobrenome);
+console.log(Idade);
+console.log(Email);
+console.log(NúmerodeCelular);
+console.log(BebidaFavorita);
+
+//Variaveis const:
+
+const dataNascimento = ("01/06/1981");
+const cpf = ("99902038369");
+
+console.log(dataNascimento);
+console.log(cpf);
+
+
+//Boolean - valor verdadeiro ou falso / true ou false
+let ClienteCadastrado = true;
+let clienteDevendo = false;
+
+//null = algo que foi definido, mas está propositalmente vazio.
+let OdiarAlguem = null;
+
+//undefined = algo que ainda não foi definido.
+let clienteContato;
+console.log(clienteContato);
